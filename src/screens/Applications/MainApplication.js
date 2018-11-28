42707b393b014bb133618f1fd1e8ae59052c6a3d
Fatima <en.fh90@gmail.com>
2018-11-29 01:15:05 +0200

    fix applStyle

--- src/screens/Applications/MainApplication.js
+++ src/screens/Applications/MainApplication.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View,TouchableOpacity, Platform} from 'react-native';
+import {View,TouchableOpacity, Platform, Text, StyleSheet} from 'react-native';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import IconAnt from 'react-native-vector-icons/AntDesign';
 
@@ -10,17 +10,35 @@ class MainApplication extends React.Component {
     
     render(){
         return(
-            <View>
-              <TouchableOpacity>
-                <IconAnt name="addfile" size={30}/>
+            <View style={styles.container}>
+              
+              <TouchableOpacity style={styles.Item}>
+                <IconAnt name="addfile" size={30} style={styles.ItemIcon}/>
+                <Text>Apply for a new application</Text>
               </TouchableOpacity>
-              <TouchableOpacity>
+
+              <TouchableOpacity style={styles.Item}>
                 <Ionicon name={Platform.OS === "android" ? "md-eye" : "ios-eye"} 
-                size={30}/>
+                size={30} style={styles.ItemIcon}/>
+                <Text>View your application</Text>
               </TouchableOpacity>
             </View>
         )
     }
 }
-  
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        alignItems: "center",
+    },
+    Item: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+    },
+    ItemIcon: {
+        marginRight: 10
+    }
+});
 export default MainApplication;
