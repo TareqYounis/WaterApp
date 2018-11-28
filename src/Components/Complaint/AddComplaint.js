82751df5ba27966ca849a28992166f891eb832c5
Fatima <en.fh90@gmail.com>
2018-11-29 00:51:31 +0200

    fix complaintStyle

--- src/Components/Complaint/AddComplaint.js
+++ src/Components/Complaint/AddComplaint.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput,Button, Picker, Platform, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput,Button, Picker, Platform, TouchableOpacity, StyleSheet } from 'react-native';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import DatePicker from 'react-native-datepicker'
 import ImagePicker from 'react-native-image-picker';
@@ -39,30 +39,23 @@ class AddComplaint extends React.Component{
 
     render(){
         return (
-            <View>
-                <Picker
-                    selectedValue={this.state.complaint_type}
-                    style={{ height: 50, width: 100 }}
-                    onValueChange={(complaint_type) => this.setState({complaint_type})}>
-                    {this.props.complaintType.map((item, index) => {
-                        return (<Picker.Item label={item.name_ar} value={item.id} key={index}/>) 
-                    })}
-                </Picker>
-
+            <View style={styles.container}>
+                <Text>Please fill the following form:</Text>
+                
                 <TextInput 
-                    style= {{width: 250, height:40, borderWidth: 2}}
+                    style= {{width: 200, height:40, borderWidth: 2}}
                     placeholder="enter your location"
                     onChangeText={(location) => this.setState({location})}
                 />
                 
                 <TextInput 
-                    style= {{width: 250, height:40, borderWidth: 2}}
+                    style= {{width: 200, height:40, borderWidth: 2}}
                     placeholder="enter your phone number"
                     onChangeText={(phone) => this.setState({phone})}
                 />
                 
                 <TextInput 
-                    style= {{width: 250, height:40, borderWidth: 2}}
+                    style= {{width: 200, height:40, borderWidth: 2}}
                     placeholder="enter your remark"
                     onChangeText={(remark) => this.setState({remark})}
                 />
@@ -76,21 +69,45 @@ class AddComplaint extends React.Component{
                     minDate="2015-05-01"
                     onDateChange={(date) => {this.setState({date: date})}}
                 />
+
+                <Picker
+                    selectedValue={this.state.complaint_type}
+                    style={{ height: 50, width: 200, borderWidth: 2 }}
+                    onValueChange={(complaint_type) => this.setState({complaint_type})}>
+                    {this.props.complaintType.map((item, index) => {
+                        return (<Picker.Item label={item.name_ar} value={item.id} key={index}/>) 
+                    })}
+                </Picker>
                 
-                <TouchableOpacity onPress={this.handleImagePick}>
-                    <View style={{flexDirection: 'row'}}>
-                    <Text>Pick an image </Text>
+                <TouchableOpacity onPress={this.handleImagePick} style={styles.Item}>
                         <Ionicon 
                         name={Platform.OS === "android" ? "md-images" : "ios-images"} 
-                        size={30}
+                        size={30} style={styles.ItemIcon}
                         />
-                    </View>
+                        <Text>Pick an image </Text>
                 </TouchableOpacity>
                
+
                 <Button title="complain" onPress={this.handleComplaint}/>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        alignItems: "center",
+    },
+    Item: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+    },
+    ItemIcon: {
+        marginRight: 10
+    }
+});
+
 
 export default AddComplaint;
