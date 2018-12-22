23b345676c63a36490981b206036395762a6bcca
Fatima <en.fh90@gmail.com>
2018-12-22 16:09:14 +0200

    feat addapp

--- src/Components/Applications/AddApplication.js
+++ src/Components/Applications/AddApplication.js
@@ -2,75 +2,143 @@ import React from 'react';
 import { View, TextInput, StyleSheet, Text, Button, DatePicker, Picker, Platform, TouchableOpacity} from 'react-native';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import IconAnt from 'react-native-vector-icons/AntDesign';
+import { connect } from 'react-redux';
+import { GetOrganizations, GetSubscriptionType, GetVillagesGIS, GetBlooksGIS, GetSectorsGIS, GetParcelsGIS } from '../../store/actions/index';
 
 class AddApplication extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+            user_id: 0,
+            sut_types: 1,
+            app_date: 0,
+            subs_category: 0,
             subs_id_number: 0,
             subs_name : '',
             mobile: '',
             address : '',
-            vil_id: 0,
+            vil_id: '825',
+            blk_id: 0,
             sec_id : 0,
-            par_id: 0
+            par_id: 0,
+            WCS_CUSTOMER_ID_PATH : '',
+            WCS_CADASTRAL_PLAN_PATH: '',
+            WCS_LOCATION_PLAN_PATH: '',
+            WCS_OCCUPANCIES_PERMIT_PATH: '',
+            WCS_REGISTERY_DOC_PATH: '',
+            home_phone_number: '',
+            street_name : '',
+            bulding_no : ''
         }
+        this.showBlocks = this.showBlocks.bind(this);
+        this.showSectors = this.showSectors.bind(this);
+        console.log('this is the props',this.props)
     }
+
+    showBlocks(){
+       if(this.state.vil_id !== 0){
+        this.props.onGetBlocksID(this.state.vil_id);
+        console.log('i tstgs',this.props.organization)
+        while (this.props.blooksGIS.length !== 0){
+            return <Picker
+                        selectedValue={this.state.blk_id}
+                        style={{ height: 50, width: 200, borderWidth: 2 }}
+                        onValueChange={( blk_id ) => this.setState({ blk_id })}>
+                        {this.props.blooksGIS.map((item, index) => {
+                            return (<Picker.Item label={item.BLK_NAME_A} value={item.BLK_ID} key={index}/>) 
+                        })}
+                    </Picker>
+        }
+        }else{
+           return null;
+       }
+    }
+
+    showSectors(){
+        <Picker
+            selectedValue={this.state.sec_id}
+            style={{ height: 50, width: 200, borderWidth: 2 }}
+            onValueChange={( sec_id ) => this.setState({ sec_id })}>
+            {this.props.sectorsGIS.map((item, index) => {
+                return (<Picker.Item label={item.SEC_NAME_A} value={item.SEC_ID} key={index}/>) 
+            })}
+        </Picker>
+    }
+
+    showPracles(){
+        <Picker
+            selectedValue={this.state.par_id}
+            style={{ height: 50, width: 200, borderWidth: 2 }}
+            onValueChange={( par_id ) => this.setState({ par_id })}>
+            {this.props.parclesGIS.map((item, index) => {
+                return (<Picker.Item label={item.PAR_ID} value={item.PAR_PUT_ID} key={index}/>) 
+            })}
+        </Picker>
+    }
+    
     render (){
         return (
             <View style={styles.container}>
-                <TextInput 
-                        style= {{width: 200, height:40, borderWidth: 2}}
-                        placeholder="enter your id number"
-                        onChangeText={(phone) => this.setState({phone})}
+                {/* <TextInput 
+                    style= {{width: 200, height:40, borderWidth: 2}}
+                    placeholder="enter your ID number"
+                    onChangeText={( subs_id_number ) => this.setState({ subs_id_number })}
                 />
                     
                 <TextInput 
-                        style= {{width: 200, height:40, borderWidth: 2}}
-                        placeholder="enter your Full name"
-                        onChangeText={(subs_name) => this.setState({subs_name})}
+                    style= {{width: 200, height:40, borderWidth: 2}}
+                    placeholder="enter your Full name"
+                    onChangeText={( subs_name ) => this.setState({ subs_name })}
                 />
 
                 <TextInput 
-                        style= {{width: 200, height:40, borderWidth: 2}}
-                        placeholder="enter your phone number"
-                        onChangeText={(mobile) => this.setState({mobile})}
+                    style= {{width: 200, height:40, borderWidth: 2}}
+                    placeholder="enter your phone number"
+                    onChangeText={( mobile ) => this.setState({ mobile })}
                 />
 
+                 <TextInput 
+                    style= {{width: 200, height:40, borderWidth: 2}}
+                    placeholder="Optional, enter your home phone number"
+                    onChangeText={( home_phone_number ) => this.setState({ home_phone_number })}
+                /> */}
+             
                 <TextInput 
-                        style= {{width: 200, height:40, borderWidth: 2}}
-                        placeholder="enter your address detailes"
-                        onChangeText={(address) => this.setState({address})}
+                    style= {{width: 200, height:40, borderWidth: 2}}
+                    placeholder="enter your address detailes"
+                    onChangeText={( address ) => this.setState({ address })}
                 />
-                    
-                {/* <Picker
-                        selectedValue={this.state.vil_id}
-                        style={{ height: 50, width: 200, borderWidth: 2 }}
-                        onValueChange={(vil_id) => this.setState({vil_id})}>
-                        {this.props.villageId.map((item, index) => {
-                            return (<Picker.Item label={item.name_ar} value={item.id} key={index}/>) 
-                        })}
-                </Picker> */}
                 
-                <DatePicker 
-                        style={{width: 200}}
-                        date={this.state.date}
-                        mode="date"
-                        placeholder="select date"
-                        format="YYYY-MM-DD"
-                        minDate="2015-05-01"
-                        onDateChange={(date) => {this.setState({date: date})}}
+                <TextInput 
+                    style= {{width: 200, height:40, borderWidth: 2}}
+                    placeholder="Optional, enter your street name"
+                    onChangeText={( street_name ) => this.setState({ street_name })}
                 />
 
-                {/* <Picker
-                    selectedValue={this.state.complaint_type}
+                <TextInput 
+                    style= {{width: 200, height:40, borderWidth: 2}}
+                    placeholder="Optional, enter your building number"
+                    onChangeText={( bulding_no ) => this.setState({ bulding_no })}
+                />
+   
+                 <Picker
+                    selectedValue={this.state.subs_category}
                     style={{ height: 50, width: 200, borderWidth: 2 }}
-                    onValueChange={(complaint_type) => this.setState({complaint_type})}>
-                    {this.props.complaintType.map((item, index) => {
+                    onValueChange={( subs_category ) => this.setState({ subs_category })}>
+                    {this.props.organization.map((item, index) => {
                         return (<Picker.Item label={item.name_ar} value={item.id} key={index}/>) 
                     })}
+                </Picker>
+
+                {/* <Picker
+                    selectedValue={this.state.vil_id}
+                    style={{ height: 50, width: 200, borderWidth: 2 }}
+                    onValueChange= {( vil_id ) => { this.setState({ vil_id }); this.showBlocks()}}>                   
+                    {this.props.villageId.map((item, index) => {
+                        return (<Picker.Item label={item.VIL_NAME_A} value={item.VIL_ID} key={index}/>) 
+                    })}
                 </Picker> */}
-                    
+                {this.showBlocks()}
                 <TouchableOpacity onPress={this.handleImagePick} style={styles.Item}>
                     <Ionicon 
                         name={Platform.OS === "android" ? "md-images" : "ios-images"} 
@@ -95,7 +163,31 @@ const styles = StyleSheet.create({
     },
     ItemIcon: {
         marginRight: 10
-    }
+    } 
 });
 
-export default AddApplication;
+const mapStateToProps = state => {
+    return {
+        data : state.enquiry.data,
+        subscriptionType: state.enquiry.subscriptionType,
+        villagesGIS: state.enquiry.villagesGIS,
+        blooksGIS: state.enquiry.blooksGIS,
+        sectorsGIS : state.enquiry.sectorsGIS,
+        parclesGIS : state.enquiry.parclesGIS,
+    }
+};
+  
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onGetOrganizations: () => dispatch(GetOrganizations()),
+        onGetSubsType: (companyId) => dispatch(GetSubscriptionType(companyId)),
+        onGetVillagesID: () => dispatch(GetVillagesGIS()),
+        onGetSectorsID: (blookId) => dispatch(GetSectorsGIS(blookId)),
+        onGetParclesID: (sectoreId) => dispatch(GetParcelsGIS(sectoreId)),
+        onGetBlocksID: (villageId) => dispatch(GetBlooksGIS(villageId))
+    };
+};
+  
+  
+export default connect(mapStateToProps,mapDispatchToProps,null, {"withRef" : true})(AddApplication);
--- src/screens/Applications/ApplyApplication.js
+++ src/screens/Applications/ApplyApplication.js
@@ -1,19 +1,62 @@
 import React from 'react';
-import {View} from 'react-native';
-import AddApplication from './../../Components/Applications/AddApplication';
+import { View } from 'react-native';
+import { connect } from 'react-redux';
+import AddApplication from '../../Components/Applications/AddApplication';
+import { GetOrganizations, GetSubscriptionType, GetVillagesGIS, GetBlooksGIS, GetSectorsGIS, GetParcelsGIS } from '../../store/actions/index';
 
 class ApplyApplication extends React.Component {
     constructor(props){
         super(props);
     }
-    
+
+    componentWillMount() {
+        this.props.onGetOrganizations();
+        this.props.onGetSubsType(),
+        this.props.onGetVillagesID();
+        this.props.onGetBlocksID();
+        this.props.onGetParclesID();
+        this.props.onGetSectorsID();
+    }
+
     render(){
+        const componentProps = {
+            organization: this.props.data,
+            subscType: this.props.subscriptionType,
+            villageId: this.props.villagesGIS,
+            blockId: this.props.blooksGIS,
+            sectorId: this.props.sectorsGIS,
+            parcelsId: this.props.parclesGIS
+        }
         return(
             <View>
-                <AddApplication />
+                <AddApplication  {...componentProps}/>
             </View>
         )
     }
 }
 
-export default ApplyApplication;
+const mapStateToProps = state => {
+    return {
+        data : state.enquiry.data,
+        subscriptionType: state.enquiry.subscriptionType,
+        villagesGIS: state.enquiry.villagesGIS,
+        blooksGIS: state.enquiry.blooksGIS,
+        sectorsGIS : state.enquiry.sectorsGIS,
+        parclesGIS : state.enquiry.parclesGIS,
+    }
+};
+  
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onGetOrganizations: () => dispatch(GetOrganizations()),
+        onGetSubsType: (companyId) => dispatch(GetSubscriptionType(companyId)),
+        onGetVillagesID: () => dispatch(GetVillagesGIS()),
+        onGetSectorsID: (blookId) => dispatch(GetSectorsGIS(blookId)),
+        onGetParclesID: (sectoreId) => dispatch(GetParcelsGIS(sectoreId)),
+        onGetBlocksID: (villageId) => dispatch(GetBlooksGIS(villageId))
+    };
+};
+  
+  
+export default connect(mapStateToProps,mapDispatchToProps,null, {"withRef" : true})(ApplyApplication);
