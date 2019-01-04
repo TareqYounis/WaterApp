4bdaa8e98a25286e4f3cc024eed492aae0e623e9
Fatima <en.fh90@gmail.com>
2019-01-04 21:17:40 +0200

    refactor addAccount

--- src/Components/Auth/AddAccount.js
+++ src/Components/Auth/AddAccount.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, TextInput, Button, Picker } from 'react-native';
+import { View, Picker, Image, StyleSheet, Text } from 'react-native';
+import Input from '../Styles/Input'
+import Button from '../Styles/Button'
 
 class AddAccount extends React.Component {
     constructor(props){
@@ -11,6 +13,7 @@ class AddAccount extends React.Component {
             account_name: ''
         }
         this.handleAddAccount = this.handleAddAccount.bind(this);
+        this.onChangeText = this.onChangeText.bind(this);
     }
 
     handleAddAccount (){
@@ -20,36 +23,67 @@ class AddAccount extends React.Component {
         this.props.onAddingAccount(this.state);
     }
     
+    onChangeText = (key, value) => {
+        this.setState({
+          [key]: value
+        })
+    }
+    
     render(){
         return(
             <View>
-                <Picker
-                    selectedValue={this.state.company_id}
-                    style={{ height: 50, width: 100 }}
-                    onValueChange={(company_id) => this.setState({company_id})}>
-                    {this.props.organizations.map((item, index) => {
-                        return (<Picker.Item label={item.name_ar} value={item.id} key={index}/>) 
-                    })}
-                </Picker> 
-                <TextInput 
-                style={{height: 40, width:300}}
-                placeholder={"enter your account"}
-                onChangeText= {(account) => this.setState({account})}
-                />
-                <TextInput 
-                style={{height: 40, width:300}}
-                placeholder={"enter your iron_number"}
-                onChangeText= {(iron_number) => this.setState({iron_number})}
-                />
-                <TextInput 
-                style={{height: 40, width:300}}
-                placeholder={"enter your account_name"}
-                onChangeText= {(account_name) => this.setState({account_name})}
-                />
-                <Button title="Add Account" onPress={this.handleAddAccount}/>
+                <View style={styles.inputContainer}>
+                    <Picker
+                        selectedValue={this.state.company_id}
+                        itemStyle={styles.picker}
+                        onValueChange={(company_id) => this.setState({company_id})}>
+                        <Picker.Item label='Please select an option...' value='0' color="#1493ff" />
+                        {this.props.organizations.map((item, index) => {
+                            return (<Picker.Item label={item.name_en} value={item.id} key={index} color="#1493ff"/>) 
+                        })}
+                    </Picker>
+                    <Input
+                        value={this.state.account}
+                        placeholder="Account Number"
+                        type='account'
+                        onChangeText={this.onChangeText}
+                    />
+                    <Input
+                        value={this.state.iron_number}
+                        placeholder="Iron Number"
+                        type='iron_number'
+                        onChangeText={this.onChangeText}
+                    />
+                    <Input
+                        value={this.state.account_name}
+                        placeholder="Account Name Holder"
+                        type='account_name'
+                        onChangeText={this.onChangeText}
+                    />
+            </View>
+            <Button
+                title='Add Account'
+                onPress={this.handleAddAccount.bind(this)}
+                isLoading={this.state.isAuthenticating}
+            />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    inputContainer: {
+      marginTop: 20
+    },
+    picker: {
+        height: 45,
+        width: 150,
+        marginBottom: 15,
+        borderBottomWidth: 1.5,
+        fontSize: 16,
+        borderBottomColor: '#1493ff',
+        fontFamily: 'Lato-Light'
+    }
+});
 
 export default AddAccount;
--- src/screens/Auth/AddUserAccount.js
+++ src/screens/Auth/AddUserAccount.js
@@ -1,44 +1,94 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import { connect } from 'react-redux';
 import { Navigation } from 'react-native-navigation';
 import { UserAddAccount, GetOrganizations } from './../../store/actions/index';
 import AddAccount from './../../Components/Auth/AddAccount';
+import { getItem } from '../../StorageData';
+
+let userID=0;
 
 class AddUserAccount extends React.Component {
     constructor(props){
         super(props);
+        this.state= {
+            isLoading: false
+        }
         this.handleAddingAccount = this.handleAddingAccount.bind(this);
     }
     //load all water companies before rendering. 
     componentWillMount() {
+        console.log(this.props.rootTag)
+        //get userID and save it locally.
+        getItem('userId')
+        .then(userid => {
+           userID = Number(userid);
+        })
         this.props.onGetOrganizations();
     }
 
     componentWillReceiveProps(props){
+        // needs to be fixed by removing the props data
         if(props.messageAddAccount){
             alert(props.messageAddAccount)
             //Navigate to the user profile screen
-            Navigation.popToRoot(props.componentId);
+            Navigation.pop(props.componentId);
+        }
+        //stop running the activity monitor when recieving message
+        if(this.props.messageFailAddAccount){
+            this.setState({
+                isLoading: !this.state.isLoading
+            })
         }
     }
 
-    handleAddingAccount(userAccount){
+    async handleAddingAccount(userAccountData){
         // send userID convery it to number
-        userAccount.user_id = parseInt(this.props.user_id, 10);
-        this.props.onAddingUserAccount(userAccount);
+        this.setState({
+            isLoading: !this.state.isLoading
+        })
+        userAccountData.user_id = userID;
+        await this.props.onAddingUserAccount(userAccountData);        
     }
 
     render(){
         return(
-            <View>
-                <Text>Add user account screen</Text>
+            <View style={styles.container}>
                 <AddAccount organizations={this.props.data} onAddingAccount={this.handleAddingAccount}/>
-                <Text>{this.props.messageFailAddAccount}</Text>
+                {this.state.isLoading && (
+                    <View style={styles.activityIndicator}>
+                        <ActivityIndicator color='#1493ff' />
+                    </View>
+                )}
+                {this.props.messageFailAddAccount && (
+                    <Text>Error Adding account, Please try again.{"\n"} {this.props.messageFailAddAccount}</Text>
+                )}
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    inputContainer: {
+      marginTop: 20
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      paddingHorizontal: 40
+    },
+    errorMessage: {
+        fontFamily: 'Lato-Regular',
+        fontSize: 12,
+        marginTop: 10,
+        color: 'transparent'
+    },
+    activityIndicator: {
+        transform: [{scale: 1.00}],
+        marginTop: 3.5,
+        marginLeft: 5
+    }
+})
 
 const mapStateToProps = state => {
     return {
