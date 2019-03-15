4c145bbcb039c42a327855bb2832324c9bb7b260
Fatima <en.fh90@gmail.com>
2019-03-15 22:30:55 +0200

    add langAction

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -2,6 +2,7 @@ export const Add_Name = "Add_Name";
 export const Delete_Name = "Delete_Name";
 export const Select_Name = "Select_Name";
 export const Deselect_Name = "Deselect_Name";
+export const Save_Lang = 'Save_Lang';
 export const Fetch_Sucess = 'Fetch_Sucess';
 export const Fetch_Failure = 'Fetch_Failure';
 export const Fetch_Sucess_Water_Role = 'Fetch_Sucess_Water_Role';
