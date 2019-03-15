3448de158ae9f791e48b8024db3451b7f1f688f3
Fatima <en.fh90@gmail.com>
2019-03-15 15:57:52 +0200

    create appTheme

---
+++ src/assets/Theme.js
@@ -0,0 +1,17 @@
+const colors = {
+    LightBlue: '#00ADD2',
+    DarkBlue: '#006DB6',
+    Green: '#4CA950'
+}
+  
+const fonts = {
+    Hacen: 'Hacen Liner XL',
+    bold: 'Hacen Tunisia Bold',
+    TunisiaLt: 'Hacen Tunisia Lt',
+    light: 'Hacen Liner Print-out Light'
+}
+  
+export {
+    colors,
+    fonts
+}  
