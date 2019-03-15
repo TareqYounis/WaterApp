810071428bc4a720c4412c5065e1caa20b45a4c8
Fatima <en.fh90@gmail.com>
2019-03-15 17:08:41 +0200

    refactor desgin

--- src/screens/StartUp/WaterCompany.js
+++ src/screens/StartUp/WaterCompany.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, Image, Dimensions, ImageBackground, TouchableOpacity } from 'react-native';
 import { Navigation } from 'react-native-navigation';
-import Button from '../../Components/Styles/Button'
+import { fonts, colors } from './../../assets/Theme';
+
+const deviceWidth = Dimensions.get("window").width;
+const deviceHeight= Dimensions.get("window").height;
 
 class WaterCompany extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
-            isAuthenticating: false,
-            company: 'Miyahuna'
-        }
         this.selectCompany = this.selectCompany.bind(this);
     }
+   
     selectCompany(company) {
         Navigation.push(this.props.componentId,{
             component:{
@@ -19,36 +19,56 @@ class WaterCompany extends React.Component {
             } 
         })
     }
+    
     render(){
         return (
             <View style={styles.container}>
-                <Button
-                    title='Miyahuna'
-                    onPress = {this.selectCompany.bind(this,'Miyahuna')}
-                    isLoading={this.state.isAuthenticating}
-                    />
-                    <Button
-                    title='Yarmook'
-                    onPress = {this.selectCompany.bind(this,'Yarmook')}
-                    isLoading={this.state.isAuthenticating}
-                    />
-                     <Button
-                    title='Aqba'
-                    onPress = {this.selectCompany.bind(this,'Aqba')}
-                    isLoading={this.state.isAuthenticating}
-                    />
-          </View>
+                <View style={styles.half1}>
+                    <Image source={require('./../../assets/images/logo_inner_page.png')} />
+                    <Text style={styles.text}>Water App</Text>
+                </View>
+                <View style={styles.half2}>
+                    <ImageBackground source={require('./../../assets/images/background_blue.png')} style={{width: deviceWidth, height: deviceHeight}} >
+                        <View style={styles.buttons}>
+                            <TouchableOpacity onPress={()=> this.selectCompany('myahuna')} style={{marginBottom: 30}}>
+                                <Image source={require('./../../assets/images/myahuna_button.png')} />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={()=> alert('لا يوجد')} style={{marginBottom: 30}}>
+                                <Image source={require('./../../assets/images/yarmouk_button.png')} />
+                            </TouchableOpacity>
+                             <TouchableOpacity onPress={()=> alert('لا يوجد')}>
+                                <Image source={require('./../../assets/images/aqaba_button.png')} />
+                            </TouchableOpacity>
+                        </View>
+                    </ImageBackground>
+                </View>
+            </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
         flex: 1
-      }
+    },
+    half1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    half2:{
+        flex: 2,
+    },
+    buttons:{
+        flex: 0.6,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text:{
+        color: colors.DarkBlue,
+        fontSize: 30,
+        fontFamily: fonts.bold
+    }
 });
 
 export default WaterCompany;
