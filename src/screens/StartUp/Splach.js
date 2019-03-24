e1b3e84b4a45af6af8efe8ce0c00aa59bd09b6c3
Fatima <en.fh90@gmail.com>
2019-03-24 05:42:08 +0200

    refactor splac

--- src/screens/StartUp/Splach.js
+++ src/screens/StartUp/Splach.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Navigation } from 'react-native-navigation';
-import { getItem } from '../../StorageData';
+import { getItem, saveParticipationInfo, saveUserAccounts } from '../../StorageData';
 import SplashScreen from 'react-native-splash-screen'
 import StartMainTabs from '../MainTabs/StartMainTabs';
+import { connect } from 'react-redux';
+import { UserParticipationInfo, SaveUserLanguage, SaveUserID } from './../../store/actions/index';
 
 class Splach extends React.Component {
     constructor(props){
         super(props);
-        this.handleNavigation= this.handleNavigation.bind(this);
     }
 
     componentWillMount(){
@@ -16,16 +17,16 @@ class Splach extends React.Component {
        .then(results => {
             if(results !== 'none') {
                 // The user is signed In, navigate to start using the app;
-                SplashScreen.hide();
-                StartMainTabs();
+                this.loadData(results);
             }else{
                 // check if this is the first time the user uses the app
                 getItem('language')
                 .then(results => {
                     // if there were a langauge, means the user has used the app from before, so navigate to sign in.
                     if(results !== 'none') {
+                        this.props.onSavingLanguage(results);
                         this.handleNavigation('water-app.LoginScreen');
-                    // otherwise, means the user has not used the app from before, so navigate to pick the langague.
+                    // otherwise, means the user has not used the app before, so navigate to pick the langague.
                     }else{
                         this.handleNavigation('water-app.LanguageScreen');
                     }
@@ -34,13 +35,38 @@ class Splach extends React.Component {
        })
     }
 
-    handleNavigation(screen){
+    handleNavigation = (screen) => {
         SplashScreen.hide();
-            Navigation.push(this.props.componentId,{
-                component:{
-                    name: screen
-                } 
-            })
+        Navigation.push(this.props.componentId,{
+            component:{
+                name: screen
+            } 
+        })
+    }
+
+    loadData = (userID) => {
+        //save userId in the state
+        this.props.onSavingUserId(userID);
+        // load all user profile data in here
+        this.props.onGetParticipationInfo(userID);
+        //save lang in the state, or save default Arabic
+        getItem('language')
+        .then(results => {
+            if(results !== 'none'){
+                this.props.onSavingLanguage(results);
+            }else{
+                this.props.onSavingLanguage('Arabic');
+            }
+        })
+    }
+
+    componentWillReceiveProps(props){
+        const partInfo = [props.particpationInfo, props.particpFailMsg];
+        // save user info (contains all his water accounts and its detailes) and accounts in phone storage
+        saveParticipationInfo(partInfo);
+        saveUserAccounts(props.userAccounts);
+        SplashScreen.hide();
+        StartMainTabs();
     }
 
     render(){
@@ -48,5 +74,21 @@ class Splach extends React.Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+      particpationInfo : state.names.particpationInfo,
+      userAccounts : state.names.userAccounts,
+      particpFailMsg : state.names.particpFailMsg
+    };
+  };
+  
 
-export default Splach;
+const mapDispatchToProps = dispatch => {
+    return {
+        onGetParticipationInfo: (userID) => dispatch(UserParticipationInfo(userID)),
+        onSavingLanguage: ( lang ) => dispatch(SaveUserLanguage(lang)),
+        onSavingUserId: ( userID ) => dispatch(SaveUserID(userID))
+    };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps,null, {"withRef" : true})(Splach);
