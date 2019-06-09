69897d23211a1dbdfbeba73b99e02c5ff42b52b7
Fatema Hammami <fatema@Fatemas-iMac.local>
2019-06-09 11:37:08 -0400

    fix fileName

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,6 @@
 export { FetchFailure, FetchSuccess, FetchSuccessWaterRoles, FetchSuccessUserRegister} from './actions';
 export { GetWaterRoles, invoiceCalculation, GetComplaintTypes } from './enquiry';
-export { SaveUserLanguage, SaveUserID, ResetState, UserSignsUp, UserLogsIn, UserAddAccount, SaveTabID, UserBalanceHistory, UserParticipationInfo, UserRegisterConfirm, UserResendCode } from './userAuth';
+export { SaveUserLanguage, SaveUserID, ResetState, UserSignsUp, UserLogsIn, UserAddAccount, SaveTabID, UserBalanceHistory, UserParticipationInfo, UserRegisterConfirm, UserResendCode } from './UserAuth';
 export { GetOrganizations, GetUsageType, GetSubscriptionType, GetVillagesGIS, GetBlooksGIS, GetSectorsGIS, GetParcelsGIS } from './waterServices';
 export { RequestApplication, ReturnBlockCounter } from './applications';
 export { Objection, Complain } from './complaints';
